42f1ef77df9aa6e2858c25483c566706985a6e99
inthinks <ind_ari08@yahoo.com>
2016-08-07 18:46:21 +0700

    delete config

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=portofolio',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
